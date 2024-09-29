7c5c88ce118ffa096fb4cd837367795814fada07
Aingar <mierzei@gmail.com>
2024-09-29 02:52:51 +0200

    Alley: Base IFF override now functional

--- Plugins/Public/alley/rep.cpp
+++ Plugins/Public/alley/rep.cpp
@@ -48,13 +48,13 @@ void REP::LoadSettings()
 					if (ini.is_value("rep"))
 					{
 						uint solarnick = CreateID(ini.get_value_string(0));
-						const char* newrep = ini.get_value_string(1);
+						uint newrep = MakeId(ini.get_value_string(1));
 
 						ConPrint(L"DEBUG: Rep of %s is %s \n", stows(ini.get_value_string(0)).c_str(), stows(ini.get_value_string(1)).c_str());
 
-						uint obj_rep_group;
-						pub::Reputation::GetReputationGroup(obj_rep_group, newrep);
-						pub::Reputation::SetAffiliation(solarnick, obj_rep_group);
+						int rep;
+						pub::SpaceObj::GetRep(solarnick, rep);
+						pub::Reputation::SetAffiliation(rep, newrep);
 					}
 				}
 			}
